1857c4630af136ab270491d3060f830f90ae8aea
Joshua Liao <137369931+Relleeee@users.noreply.github.com>
2026-04-12 17:12:26 +0800

    Changes made
    Wordings

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 // Typed JS
 
 const typed = new Typed('.multiple-text', {
-    strings: ['A Software Engineering Student | Full-Stack Developer | UX & UI Designer'],
+    strings: ['A Software Engineering | Full-Stack Developer | UX & UI Designer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
